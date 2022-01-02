bffc32ada1e38bf079a47dd95e27d3aa8c784484
Illusion of control <sergeyskorokhod2@gmail.com>
2022-01-02 15:00:56 +0300

    fixed user repository

--- monologue/repositories/user.py
+++ monologue/repositories/user.py
@@ -4,20 +4,24 @@ from monologue import db
 class UserRepository():
     def check_username_is_taken(self, username) -> bool:
         exist_query = User.query.filter_by(username=username).exists()
-        if db.session.query(exist_query):
+        if db.session.query(exist_query).scalar():
             return True
         return False
 
     def check_email_is_taken(self, email) -> bool:
         exist_query = User.query.filter_by(email=email).exists()
-        if db.session.query(exist_query):
+        if db.session.query(exist_query).scalar():
             return True
         return False
 
-    def create_user(self, username, email, password) -> User:
-        return User(username=username,
+    def create_user(self, username, email, password, with_commit=True) -> User:
+        user = User(username=username,
                 email=email,
                 password=email)
+        db.session.add(user)
+        if with_commit:
+            db.session.commit()
+        return user
 
     def get_user_by_username(self, username) -> User:
         return User.query.filter_by(username=username).one()
